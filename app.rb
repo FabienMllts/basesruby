9fc0ccecd71cc365eae77de96cdde530ce75e804
FabienMllts <fabienmallet@gmail.com>
2018-01-23 15:29:02 +0100

    avec le each

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
  
 
- 10.times do
- 	puts"Hello World"
- end
+10.times do
+	puts"Hello World"
+end
 
 
 i = 0
 while i < 10
 	puts "Hi World"
- i+=1
+	i+=1
 end
 
 puts "Hi world \n" *10
@@ -18,5 +18,6 @@ for i in 0...10
 	puts "Hello"
 end
 
-t = [1...10]
-t.each do |h| puts "hello1 \n"*10  end
+t = (1..10)
+t.each do |h|
+	puts "Hallo" end
