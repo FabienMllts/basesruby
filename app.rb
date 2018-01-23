ea9bab4a67428563a9708cdc7c0e9a637b5d6137
FabienMllts <fabienmallet@gmail.com>
2018-01-23 15:24:01 +0100

    10 times4 times

--- app.rb
+++ app.rb
@@ -1,3 +1,22 @@
+ 
+
  10.times do
  	puts"Hello World"
  end
+
+
+i = 0
+while i < 10
+	puts "Hi World"
+ i+=1
+end
+
+puts "Hi world \n" *10
+
+
+for i in 0...10
+	puts "Hello"
+end
+
+t = [1...10]
+t.each do |h| puts "hello1 \n"*10  end
