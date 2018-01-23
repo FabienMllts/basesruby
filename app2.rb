1ae5b39b158699322724b3b84195761df9acfa11
FabienMllts <fabienmallet@gmail.com>
2018-01-23 17:47:58 +0100

    Array prime numbers

--- app2.rb
+++ app2.rb
@@ -1,11 +1,22 @@
 def is_prime(n)
 	diviseurs = []
 	for diviseur in 1..n
-		puts "#{n} modulo(%) #{diviseur} = " + (n % diviseur).to_s
+		#puts "#{n} modulo(%) #{diviseur} = " + (n % diviseur).to_s
 		diviseurs << diviseur if (n % diviseur == 0)
 	end
-	p diviseurs
+	# p diviseurs
 	return diviseurs.length == 2
 end
 
-p is_prime(11)
+# p is_prime(10)
+
+i = 0
+primes = [] 
+while i < 51 
+	p is_prime(i)	
+	primes << i if is_prime(i)
+	puts i += 1
+end
+p primes
+
+
